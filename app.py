3003d7cb7ac0585e3452e57d114273ba808510f5
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-04-03 06:58:22 +0000

    Add post methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,17 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {'name': request_data['name'], 'items': []}
+    stores.append(new_store)
+    # return 201 as response code that says I've accepted your payload and processed it successfully
+    return new_store, 201
+
+
+@app.post('store/<string:name>/item') # string:name allows us to access name var in the URL
+def create_item(name):
+    pass
