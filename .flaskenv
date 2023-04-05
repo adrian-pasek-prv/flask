61d7dd877fed5311ea6cd7a87d93368db4b98038
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-04-05 05:50:52 +0000

    Restructure a data model

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,5 @@
+# These variables will be used when running flask app
+# .flaskenv will be available for flask app because we have python-dotenv in requirements.txt
+# FLASK_DEBUG=1 allows for autoreload of an app after changes
 FLASK_APP=app
-FLASK_ENV=development
+FLASK_DEBUG=1
--- app.py
+++ app.py
@@ -1,53 +1,51 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "my item",
-                "price": 15.99
-            }
-        ]
-    }
-]
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {'name': request_data['name'], 'items': []}
-    stores.append(new_store)
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, 'id': store_id}
+    stores[store_id] = store
     # return 201 as response code that says I've accepted your payload and processed it successfully
-    return new_store, 201
+    return store, 201
 
 
-@app.post('/store/<string:name>/item') # string:name allows us to access name var in the URL
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {'name': request_data['name'], 'price': request_data['price']}
-            store['items'].append(new_item)
-            return new_item, 201
-    return {'message": "Store not found'}, 404
+@app.post('/item')
+def create_item():
+    item_data = request.get_json()
+    if item_data['store_id'] not in stores:
+        return {'message': 'Store not found'}, 404
+   
+    item_id = uuid.uuid4().hex
+    item = {**item_data, 'id': item_id}
+    items[item_id] = item
+    
+    return item, 201
 
-@app.get('/store/<string:name>') # string:name allows us to access name var in the URL
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return store
-    return {'message": "Store not found'}, 404
+@app.get("/item")
+def get_items():
+    return {"items": list(items.values())}
 
-@app.get('/store/<string:name>/item') # string:name allows us to access name var in the URL
-def get_store_items(name):
-    for store in stores:
-        if store['name'] == name:
-            return {'items': store['items']}
-    return {'message": "Store not found'}, 404
+@app.get('/store/<string:store_id>') # string:name allows us to access name var in the URL
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {'message": "Store not found'}, 404
+
+@app.get('/item/<string:item_id>') # string:name allows us to access name var in the URL
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {'message": "Item not found'}, 404
