bb962c4c3fa39178712803c4981a5ca1be4ce298
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-04-11 06:16:39 +0000

    Setup schemas.py using marshmellow for data validation

---
+++ schemas.py
@@ -0,0 +1,23 @@
+# Marshmellow is a tool that enables data validation using Schemas
+from marshmellow import Schema, fields
+
+# Config an ItemSchema class that will dictate how an item should look like
+class ItemSchema(Schema):
+    # dump_only means this field will only be returned
+    # it not required as a parameter in request
+    id = fields.Str(dump_only=True)
+    # required field in the request
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
+    
+class ItemUpdateSchema(Schema):
+    # These fields are optional
+    name = fields.Str()
+    price = fields.Float()
+    
+class StoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    
+    
