f89192e25918367c41c47882eaaac8d9993b72df
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-04-12 05:55:53 +0000

    Restructure schemas.py to include nested fields of relationships

--- schemas.py
+++ schemas.py
@@ -1,23 +1,36 @@
 # Marshmellow is a tool that enables data validation using Schemas
 from marshmallow import Schema, fields
 
-# Config an ItemSchema class that will dictate how an item should look like
-class ItemSchema(Schema):
+
+# Config an PlainItemSchema class that will dictate how an item should look like
+# in its simplest form
+class PlainItemSchema(Schema):
     # dump_only means this field will only be returned
     # it not required as a parameter in request
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     # required field in the request
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
-    
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+# This schema will inherit from PlainItemSchema
+# and add additional fields related to store of an item
+# Store metadata will exists in a nested field that will be populated by PlainStoreSchema
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
 class ItemUpdateSchema(Schema):
     # These fields are optional
     name = fields.Str()
     price = fields.Float()
-    
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
-    
-    
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
