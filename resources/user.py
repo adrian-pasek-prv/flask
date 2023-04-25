ec81fe5b519a8bf1a94947c6caaf61192ee0205f
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-04-25 06:10:45 +0000

    Configure refresh tokens

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
 from blocklist import BLOCKLIST
 
 from db import db
@@ -42,10 +42,23 @@ class UserLogin(MethodView):
         
         # Check if you can match hashed version of provided password to hashed version stored in DB
         if user and pbkdf2_sha256.verify(user_data['password'], user.password):
-            access_token = create_access_token(identity=user.id)
-            return {'access_token': access_token}
+            # Access token will be used for critical endpoints, while
+            # refresh token to not so important endpoints
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return {'access_token': access_token, 'refresh_token': refresh_token}
         
         abort(401, message='Invalid credentials.')
+
+@blp.route('/refresh')
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        # fresh=Flase is important because otherwise we will overwrite fresh token that is used for critical operations
+        # that require user to reauthenticate
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}
         
 @blp.route('/logout')
 class UserLogout(MethodView):
@@ -56,7 +69,8 @@ class UserLogout(MethodView):
         BLOCKLIST.add(jti)
         return {'message': 'Successfully logged out.'}
             
-            
+
+@jwt_required()           
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
     """
